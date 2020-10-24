c30d0c089156d03a3d65225f83819ddaadea1a93
mohit <mohitkc44@gmail.com>
2020-10-24 04:38:58 -0400

    Updating new feature, fix auto layout

--- InstaG/logInViewController.swift
+++ InstaG/logInViewController.swift
@@ -7,9 +7,46 @@
 //
 
 import UIKit
+import Parse
 
 class logInViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password:password) {
+          (user: PFUser?, error: Error?) in
+          if user != nil {
+            self.performSegue(withIdentifier: "logInSegue", sender: nil)
+          } else {
+            print("The login failed.")
+          }
+        }
+
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "logInSegue", sender: nil)
+            } else{
+                print("error signing in")
+            }
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
